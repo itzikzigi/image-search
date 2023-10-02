38135e6245014e8a5810e2c9c284cb74c18a0496
itzikzigi <itsikzigi@gmail.com>
2023-10-03 00:06:34 +0300

    try4

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,38 +1,38 @@
-import { FormEvent, useRef, useState } from "react";
-import { Form } from "react-bootstrap";
-import Filters from "./Filters";
-import fetchIMages from "../utils/fetch";
+// import { FormEvent, useRef, useState } from "react";
+// import { Form } from "react-bootstrap";
+// import Filters from "./Filters";
+// import fetchIMages from "../utils/fetch";
 
-const SearchForm = () => {
-  const searchInput = useRef(null);
-  const [images, setImages] = useState();
-  const [totalPages, setTotalPages] = useState();
-  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    fetchIMages(searchInput.current.value)
-      .then((data) => {
-        setImages(data.results);
-        setTotalPages(data.total_pages);
-        console.log(images);
-      })
-      .catch((error) => console.log(error));
-    console.log(searchInput.current.value);
-  };
-  return (
-    <div className="container">
-      <h1 className="title">Image Search</h1>
-      <div className="search-section">
-        <Form onSubmit={handleSearch}>
-          <Form.Control
-            type="search"
-            placeholder="Type something to search..."
-            className="search-input"
-            ref={searchInput}
-          />
-        </Form>
-      </div>
-      <Filters />
-    </div>
-  );
-};
-export default SearchForm;
+// const SearchForm = () => {
+//   const searchInput = useRef(null);
+//   const [images, setImages] = useState();
+//   const [totalPages, setTotalPages] = useState();
+//   const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     fetchIMages(searchInput.current.value)
+//       .then((data) => {
+//         setImages(data.results);
+//         setTotalPages(data.total_pages);
+//         console.log(images);
+//       })
+//       .catch((error) => console.log(error));
+//     console.log(searchInput.current.value);
+//   };
+//   return (
+//     <div className="container">
+//       <h1 className="title">Image Search</h1>
+//       <div className="search-section">
+//         <Form onSubmit={handleSearch}>
+//           <Form.Control
+//             type="search"
+//             placeholder="Type something to search..."
+//             className="search-input"
+//             ref={searchInput}
+//           />
+//         </Form>
+//       </div>
+//       <Filters />
+//     </div>
+//   );
+// };
+// export default SearchForm;
