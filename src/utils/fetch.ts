d7ef4e363f3da9194f17c36be03caaead9713bfc
itzikzigi <itsikzigi@gmail.com>
2023-10-03 00:12:08 +0300

    update key

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const key = import.meta.env.VITE_API_KEY;
+// const key = import.meta.env.VITE_API_KEY;
+const key = "Ksy1s7IlgZp4qTRlIN1EOD0Jb-0IholZq_FuNc8Pgo0";
 const apiURL = "https://api.unsplash.com/search/photos";
 const maxImagesPerPage = 20;
 
